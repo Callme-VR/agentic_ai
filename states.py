4a6b0019afe9d3b6c45b6b49f60b5260f9c53ba2
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-07-05 15:53:35 +0530

    add states and decorator

--- states.py
+++ states.py
@@ -10,11 +10,21 @@ class States(TypedDict):
      # create the blue print of the states
      topic:str
      summary:str
-     score:str
+     score:int
      
 # pydantic model
 # it is good in runtime and data validation and type checking
 
 from pydantic import BaseModel , field_validator
 
+class states(BaseModel):
+     topic:str
+     summary:str=""
+     score:int
+     
+     @field_validator('score')
+     def score_positive(cls,v):
+          if v<0:
+               raise ValueError("Score must be positive")
+          return v
 
